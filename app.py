87cbb185b9a1de9bc1805b7918276f9c0833d54f
leesol <jbkljo@naver.com>
2023-06-02 00:40:52 +0900

    feat: 사용자 채팅만들기

--- app.py
+++ app.py
@@ -21,9 +21,6 @@ with open('style.css') as f:
 # 커스텀 레이아웃 생성
 my_container1 =  st.container()
 
-# # 텍스트 입력 필드 추가
-# text_input = my_container.text_input("텍스트 입력")
-
 # 이미지 표시
 my_container1.image("chat.png", "안녕하세요!")
 my_container1.image("chat.png", "어떤것을 알고 싶으신가요?")
@@ -31,51 +28,18 @@ my_container1.image("chat.png", "어떤것을 알고 싶으신가요?")
 
 
 
-# # 사용자 입력에 따라 응답을 생성하는 함수
-# def generate_response(input_text):
-#     # 적절한 응답 생성 로직을 구현합니다.
-#     response = "안녕하세요! 입력한 메시지는: " + input_text
-#     return response
 
-# # Streamlit 애플리케이션 설정
-
-# my_container2 = st.container()
-
-# with my_container2:
-#     user_input = st.text_input("", "")
+# 사용자 입력에 따라 응답을 생성하는 함수
+def generate_response(input_text):
+    # 적절한 응답 생성 로직을 구현합니다.
+    response = input_text
+    return my_container1.image("person.png", response)
 
 
-# with my_container2:
-#     if st.button("전송"):
-#         response = generate_response(user_input)
-#         st.text("응답:")
-#         st.text(response)
-
-# # 컨테이너 1에 대한 스타일을 적용하는 CSS 클래스 이름
-# container1_class = "my-container1"
 
 
-# 컨테이너 2에 대한 스타일을 적용하는 CSS 클래스 이름
-container2_class = "my-container2"
 
-# # 컨테이너 1 생성
-# my_container1 = st.container()
-# # 컨테이너 1에 클래스 이름을 적용하여 스타일 적용
-# my_container1.markdown(
-#     f'<div class="{container1_class}">'
-#     f'<img src="chat.png" alt="안녕하세요!">'
-#     f'<img src="chat.png" alt="어떤것을 알고 싶으신가요?">'
-#     f'</div>',
-#     unsafe_allow_html=True
-# )
+user_input = st.text_input("", "")
 
-
-# 두 번째 컨테이너 스타일을 적용하는 CSS 클래스
-container2_class = "my-container2"
-st.markdown(
-    f'<div class="{container2_class}">'
-    f'<input placeholder="ask something"></input>'
-    f'<button>▼</button>'
-    f'</div>',
-    unsafe_allow_html=True
-)
+if st.button("전송"):
+    response = generate_response(user_input)
